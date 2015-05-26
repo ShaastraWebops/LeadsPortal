ce822449d8313c47a7fb01ebfaeb7adc05c9ccd7
raakeshkamal <raakeshkamal@gmail.com>
2015-05-27 01:23:14 +0530

    made the bootstrap modal work

--- client/app/leadsPortal/deal/dealController.js
+++ client/app/leadsPortal/deal/dealController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('erp2015App').controller('dealController', function ($scope, Auth, LeadsPortalService, $stateParams, $http, $q) {
+angular.module('erp2015App').controller('dealController', function ($scope, $state,Auth, LeadsPortalService, $stateParams, $http, $q) {
     $scope.selectedCoords = [];
     $scope.update = {};
 	$scope.coordsIds = [];
@@ -26,34 +26,33 @@ angular.module('erp2015App').controller('dealController', function ($scope, Auth
 			// do some error handling here
 			console.log(err);
 		});
-	});
 
-	// $scope.newUpdate = function (form) {
-	// 	$scope.submitted = true;
-	// 	angular.forEach($scope.selectedCoords, function (item) {
-	// 		$scope.coordsIds.push(item._id);
-	// 	});
-	// 	LeadsPortalService.createUpdate({
-	// 		title: $scope.update.title,
-	// 		summary: $scope.udpate.summary,
-	// 		pointOfContactName: $scope.update.poc_name,
-	// 		pointOfContactNumber: $scope.update.poc_phone,
-	// 		pointOfContactEmail: $scope.update.poc_email,
-	// 		assignees: $scope.coordsIds,
-	// 		deal: $stateParams.id,
-	// 	})
-	// 	.success(function (data) {
-	// 		$state.go('deal');
-	// 	})
-	// 	.error(function (err) {
-	// 		err = err.data;
-	// 		$scope.errors = {};
-
-	//         // Update validity of form fields that match the mongoose errors
-	//         angular.forEach(err.errors, function (error, field) {
-	//         	form[field].$setValidity('mongoose', false);
-	//           	$scope.errors[field] = error.message;
-	//         });
- //        });
-	// };
-//});
+	 $scope.newUpdate = function (form) {
+	 	$scope.submitted = true;
+	 	angular.forEach($scope.selectedCoords, function (item) {
+	 		$scope.coordsIds.push(item._id);
+	 	});
+	 	console.log('there');
+	 	LeadsPortalService.createUpdate({
+	 		title: $scope.update.title,
+	 		summary: $scope.update.summary,
+	 		pointOfContactName: $scope.update.poc_name,
+	 		pointOfContactNumber: $scope.update.poc_phone,
+	 		pointOfContactEmail: $scope.update.poc_email,
+	 		assignees: Auth.getCurrentUser()._id,
+	 		deal: $stateParams.id
+	 	})
+	 	.then(function (data) {
+	 		$state.go('deal');
+	 	})
+	 	.catch(function (err) {
+	 		err = err.data;
+	 		$scope.errors = {};
+	         // Update validity of form fields that match the mongoose errors
+/*	         angular.forEach(err.errors, function (error, field) {
+	         	form[field].$setValidity('mongoose', false);
+	           	$scope.errors[field] = error.message;
+	         });*/
+         });
+    };
+});
