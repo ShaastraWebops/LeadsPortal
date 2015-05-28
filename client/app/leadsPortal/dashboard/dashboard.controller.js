a1ea284f647e9a64fa30f9264ad6af80f04bf842
chinnichaitanya <chchaitanya95@gmail.com>
2015-05-29 04:51:22 +0530

    format code

--- client/app/leadsPortal/dashboard/dashboard.controller.js
+++ client/app/leadsPortal/dashboard/dashboard.controller.js
@@ -1,14 +1,11 @@
-    'use strict';
+'use strict';
 
 angular.module('erp2015App')
-  .controller('DashboardCtrl', function ($scope,$http,$stateParams, Auth, LeadsPortalService) {
-    	  
+  .controller('DashboardCtrl', function ($scope, $http, $stateParams, Auth, LeadsPortalService) {  
     	$scope.myDeals = [];  
     	LeadsPortalService.getDealsAssigned()
     		.then(function (deals) {
     			console.log(deals);
     			$scope.myDeals = deals;
     		});
-
-
   });
