a3241c58a4c18d5bc918d9c042e53922883b6fcd
chinnichaitanya <chchaitanya95@gmail.com>
2015-05-28 23:10:59 +0530

    remove unnecessary line in allDeals.controller

--- client/app/leadsPortal/allDeals/allDeals.controller.js
+++ client/app/leadsPortal/allDeals/allDeals.controller.js
@@ -2,7 +2,6 @@
 
 angular.module('erp2015App')
   .controller('AllDealsCtrl', function ($scope, Auth, $http, LeadsPortalService, $stateParams) {
-    console.log($scope.map);
     	$scope.allDeals = [];
     	LeadsPortalService.getAllDeals()
     		.then(function (allDeals) {
