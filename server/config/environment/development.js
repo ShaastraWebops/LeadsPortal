2d0d1a75ebd128f5fb155497abe4a0669881f79c
chinnichaitanya <chchaitanya95@gmail.com>
2015-06-04 00:11:13 +0530

    change database name

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/erp2015-dev'
+    uri: 'mongodb://localhost/erp2015-LeadsPortal-dev'
   },
 
   seedDB: false
