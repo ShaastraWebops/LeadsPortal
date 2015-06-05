23f55d2dbd31675414d2f899ac6b8b47d4e7cc49
chinnichaitanya <chchaitanya95@gmail.com>
2015-06-05 06:38:57 +0530

    change frontend

--- client/app/leadsPortal/allDeals/allDeals.controller.js
+++ client/app/leadsPortal/allDeals/allDeals.controller.js
@@ -1,10 +1,13 @@
 	'use strict';
 
 angular.module('erp2015App')
-  .controller('AllDealsCtrl', function ($scope, Auth, $http, LeadsPortalService, $stateParams) {
+  .controller('AllDealsCtrl', function ($scope, $state, Auth, $http, LeadsPortalService, $stateParams) {
     	$scope.allDeals = [];
     	LeadsPortalService.getAllDeals()
     		.then(function (allDeals) {
     			$scope.allDeals = allDeals;
     		});
+    	$scope.gotoDeal = function (deal) {
+    		$state.go('deal', {id: deal._id});
+    	}
   });
