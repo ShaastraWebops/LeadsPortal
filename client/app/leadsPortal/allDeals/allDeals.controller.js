3496d9c38be476c4b27666cb7708427070949a8a
chinnichaitanya <chchaitanya95@gmail.com>
2015-06-05 06:53:34 +0530

    bug fix in deals.controller in client side

--- client/app/leadsPortal/allDeals/allDeals.controller.js
+++ client/app/leadsPortal/allDeals/allDeals.controller.js
@@ -2,12 +2,12 @@
 
 angular.module('erp2015App')
   .controller('AllDealsCtrl', function ($scope, $state, Auth, $http, LeadsPortalService, $stateParams) {
-    	$scope.allDeals = [];
-    	LeadsPortalService.getAllDeals()
-    		.then(function (allDeals) {
-    			$scope.allDeals = allDeals;
-    		});
-    	$scope.gotoDeal = function (deal) {
-    		$state.go('deal', {id: deal._id});
-    	}
+  	$scope.allDeals = [];
+  	LeadsPortalService.getAllDeals()
+  		.then(function (allDeals) {
+  			$scope.allDeals = allDeals;
+  		});
+  	$scope.gotoDeal = function (deal) {
+  		$state.go('deal', {id: deal._id});
+  	}
   });
--- client/app/leadsPortal/dashboard/dashboard.controller.js
+++ client/app/leadsPortal/dashboard/dashboard.controller.js
@@ -2,9 +2,12 @@
 
 angular.module('erp2015App')
   .controller('DashboardCtrl', function ($scope, $http, $stateParams, Auth, LeadsPortalService) {  
-    	$scope.myDeals = [];  
-    	LeadsPortalService.getDealsAssigned()
-    		.then(function (deals) {
-    			$scope.myDeals = deals;
-    		});
+   	$scope.myDeals = [];  
+   	LeadsPortalService.getDealsAssigned()
+   		.then(function (deals) {
+   			$scope.myDeals = deals;
+   		});
+   	$scope.gotoDeal = function (deal) {
+   		$state.go('deal', {id: deal._id});
+   	}    	
   });
