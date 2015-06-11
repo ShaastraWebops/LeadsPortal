1bedbcbba966683845b2b000c49357beb9ccc299
chinnichaitanya <chchaitanya95@gmail.com>
2015-06-11 19:34:16 +0530

    indent code for sorting deals and optimize it

--- client/app/leadsPortal/allDeals/allDeals.controller.js
+++ client/app/leadsPortal/allDeals/allDeals.controller.js
@@ -1,70 +1,57 @@
-	'use strict';
+'use strict';
 
 angular.module('erp2015App')
-  .controller('AllDealsCtrl', function ($scope, $filter, $state, Auth, $http, LeadsPortalService, $stateParams) {
-  	
-   LeadsPortalService.getCoords()
-  .then(function (data) {
-    $scope.coords = data;
-  },function (err){
-     console.log(err);
-  });
-   
-   $scope.selectedCoords = [];
-   
-   $scope.verticals = [];
-   $scope.verticals = LeadsPortalService.verticals;
+  .controller('AllDealsCtrl', function ($scope, $filter, $state, Auth, LeadsPortalService) {
 
-   $scope.selectedVerticals = [];
+  $scope.allDeals = [];
+  $scope.sortedDeals = [];
+  $scope.selectedCoords = [];
+  $scope.verticals = [];
+  $scope.selectedVerticals = [];
 
-   var sortedVerticals = [];
-   var sortedCoords = [];
-  
-  $scope.search=function() {
-    var verticalSelected = [];
-   angular.forEach($scope.selectedVerticals, function (item) {
-         verticalSelected.push(item.name);
-      });
-    var coordSelected = [];
-   angular.forEach($scope.selectedCoords, function (item) {
-         coordSelected.push(item.name);
+  LeadsPortalService.getAllDeals()
+    .then(function (allDeals) {
+      $scope.allDeals = allDeals;
+      $scope.sortedDeals = allDeals;
+    }, function (err) {
+       console.log(err);
     });
-
-     sortedVerticals = [];
-     sortedCoords = [];
-
-      $scope.allDeals = [];
-      var sortedDeals = [];
-
-      LeadsPortalService.getAllDeals()
-      .then(function (allDeals) {
-   angular.forEach(verticalSelected, function (item) { 
-        var expression =  { 
-               vertical: { name: item }
-          };
-        sortedVerticals = sortedVerticals.concat($filter('filter')(allDeals, expression));
-        });
-   angular.forEach(coordSelected, function (item) { 
-      var expression =  { 
-               assignees: { name: item }
-          };
-        sortedCoords = sortedCoords.concat($filter('filter')(allDeals, expression));
-        });
-      sortedDeals = _.union(sortedVerticals, sortedCoords);
-      $scope.allDeals = sortedDeals;
-      if (sortedDeals.length == 0){
-         $scope.allDeals = allDeals;
-          };
-      });
-    };   
+    
+  LeadsPortalService.getCoords()
+    .then(function (data) {
+      $scope.coords = data;
+    }, function (err) {
+       console.log(err);
+    });
    
-   $scope.allDeals = [];
-    LeadsPortalService.getAllDeals()
-      .then(function (allDeals) {
-        $scope.allDeals = allDeals;
-      });
+  $scope.verticals = LeadsPortalService.verticals;
+  
+  $scope.search = function() {
+    var verticalSelected = $scope.selectedVerticals;
+    var coordSelected = $scope.selectedCoords;
 
+    var sortedVerticals = [];
+    var sortedCoords = [];
+    var sortedDeals = [];
+
+    var allDeals = $scope.allDeals;
+    angular.forEach(verticalSelected, function (item) { 
+      var expression = { vertical: { name: item.name } };
+      sortedVerticals = sortedVerticals.concat($filter('filter')(allDeals, expression));
+    });
+    angular.forEach(coordSelected, function (item) { 
+      var expression = { assignees: { name: item.name } };
+      sortedCoords = sortedCoords.concat($filter('filter')(allDeals, expression));
+    });
+    if (verticalSelected.length != 0 || coordSelected.length != 0) {
+      $scope.sortedDeals = _.union(sortedVerticals, sortedCoords);
+    } else {
+      $scope.sortedDeals = allDeals;
+    };
+  };   
+  
   $scope.gotoDeal = function (deal) {
-  		$state.go('deal', {id: deal._id});
-  	}
-  });
+  	$state.go('deal', {id: deal._id});
+  };
+
+});
