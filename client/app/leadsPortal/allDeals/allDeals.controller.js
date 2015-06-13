a2bab02ce4119b0857a8f636ee02c6e2fe9f2124
Sriram <sriramdommeti169@gmail.com>
2015-06-14 02:20:13 +0530

    fix search by text in all deals

--- client/app/leadsPortal/allDeals/allDeals.controller.js
+++ client/app/leadsPortal/allDeals/allDeals.controller.js
@@ -31,7 +31,7 @@ angular.module('erp2015App')
     });
    
   $scope.verticals = LeadsPortalService.verticals;
-  
+
   $scope.search = function() {
     sessionStorage.clear();
     var verticalSelected = $scope.selectedVerticals;
@@ -39,6 +39,7 @@ angular.module('erp2015App')
 
     var sortedVerticals = [];
     var sortedCoords = [];
+    var sortedText = [];
     var sortedDeals = [];
    
     $scope.dealsTitle = "List of required Deals";
@@ -52,8 +53,11 @@ angular.module('erp2015App')
       var expression = { assignees: { name: item.name } };
       sortedCoords = sortedCoords.concat($filter('filter')(allDeals, expression));
     });
-    if (verticalSelected.length != 0 || coordSelected.length != 0) {
-      $scope.sortedDeals = _.union(sortedVerticals, sortedCoords);
+    if (String($scope.searchText).length != 0) {
+      sortedText = $filter('filter')(allDeals, String($scope.searchText));
+    };
+    if (verticalSelected.length != 0 || coordSelected.length != 0 || sortedText.length != 0) {
+      $scope.sortedDeals = _.union(sortedVerticals, sortedCoords, sortedText);
     } else {
       $scope.sortedDeals = allDeals;
       $scope.dealsTitle = "List of all Deals";
@@ -62,16 +66,19 @@ angular.module('erp2015App')
     var storageObject = {
      "storedVerticals": verticalSelected,
      "storedCoords": coordSelected,
+     "storedText": String($scope.searchText),
      "storedDeals": $scope.sortedDeals
     };
     sessionStorage.setItem("storedVerticals", JSON.stringify(storageObject.storedVerticals));
     sessionStorage.setItem("storedCoords", JSON.stringify(storageObject.storedCoords));
+    sessionStorage.setItem("storedText", storageObject.storedText);
     sessionStorage.setItem("storedDeals", JSON.stringify(storageObject.storedDeals));
   };  
   function populateStorage() {
     $scope.sortedDeals = JSON.parse(sessionStorage.getItem("storedDeals"));
     $scope.selectedVerticals = JSON.parse(sessionStorage.getItem("storedVerticals"));
     $scope.selectedCoords = JSON.parse(sessionStorage.getItem("storedCoords"));
+    $scope.searchText = sessionStorage.getItem("storedText");
     $scope.dealsTitle = "List of required Deals";
   };
   
