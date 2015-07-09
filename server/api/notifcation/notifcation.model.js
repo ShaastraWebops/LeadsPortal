5e88ec7a3017135b47b7ce64de3186a83e2d7722
chinnichaitanya <chchaitanya95@gmail.com>
2015-07-10 04:27:43 +0530

    add deepPopulate plugin to notifications-server side

--- server/api/notifcation/notifcation.model.js
+++ server/api/notifcation/notifcation.model.js
@@ -1,12 +1,31 @@
 'use strict';
 
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var deepPopulate = require('mongoose-deep-populate');
 
 var NotifcationSchema = new Schema({
   name: String,
   info: String,
-  active: Boolean
+  deals: [{ type: Schema.Types.ObjectId, ref: 'Deal' }],
+  updates: [{ type: Schema.Types.ObjectId, ref: 'Update' }]
+});
+
+NotifcationSchema.plugin(deepPopulate, {
+  populate: {
+    'updates.createdBy': {
+      select: 'name rollNumber email phoneNumber'
+    },
+    'updates.lastEditedBy': {
+      select: 'name rollNumber email phoneNumber'
+    },
+    'deals.createdBy': {
+      select: 'name rollNumber email phoneNumber'
+    },
+    'deals.lastEditedBy': {
+      select: 'name rollNumber email phoneNumber'
+    }
+  }	
 });
 
 module.exports = mongoose.model('Notifcation', NotifcationSchema);
