05f00a46c2035527b54ab5429c918c694e3822be
chinnichaitanya <chchaitanya95@gmail.com>
2015-09-23 01:36:09 +0530

    fix bug

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -15,7 +15,7 @@ angular.module('erp2015App')
         })
         .then( function() {
           // Logged in, redirect to home
-          $state.go('/');
+          $state.go('main');
         })
         .catch( function(err) {
           $scope.errors.other = err.message;
